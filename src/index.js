126f0824803bf72a91e11d16b3a63f3b42a3482d
kelly_maass <kmaass77627@gmail.com>
2024-05-28 12:17:36 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-let addToy = false;
-
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
-});
+
+fetch('http://localhost:3003/toys')
+.then((response) => response.json())
+.then((json) => makeToy(json))
+
+
+function makeToy(toys) {
+  let collection = document.getElementById("toy-collection")
+  for (let toy of toys) {
+   let toyCard = document.createElement('div')
+   toyCard.className = "card"
+   let h2 = document.createElement('h2')
+   h2.innerHTML = toy.name
+   let image = document.createElement('img')
+   image.className = "toy-avatar"
+   let sum = document.createElement('p')
+   let button = document.createElement('button')
+   button.className = "like-btn"
+   button.id == toy.id
+   sum.innerHTML = toy.likes
+   image.src = toy.image
+   toyCard.appendChild(h2)
+   toyCard.appendChild(image)
+   toyCard.appendChild(sum)
+   toyCard.appendChild(button)
+   collection.appendChild(toyCard)
+  }
+
+
+  
+}
+
+
+
+
+
+
+
+})
+
+
+
+
+
